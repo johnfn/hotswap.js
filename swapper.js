d4f3e4ae7d7cdcfd54fc7498c08099ff5114a89e
johnfn <johnfn@gmail.com>
2014-04-30 23:11:32 -0700

    Convert tabs to spaces.

--- swapper.js
+++ swapper.js
@@ -4,30 +4,30 @@ var uris = [];
 var loaded_scripts = {};
 
 for (var i = 0; i < scripts.length; i++) {
-	uris.push(scripts[i].getAttribute('src'));
+  uris.push(scripts[i].getAttribute('src'));
 }
 
 uris = ["test.js"];
 
 var instrument = function(script) {
-	var syntax = esprima.parse('var answer = 42');
+  var syntax = esprima.parse('var answer = 42');
 };
 
 var scan = function() {
-	for (var i = 0; i < uris.length; i++) {
-		var uri = uris[i];
-		var script = $.ajax({url: uri, async: false, dataType: 'text' }).responseText; // Fun fax: if you don't flag it as text, jQuery will "intelligently" assume it to be javascript and recursively eval the same file over and over until the browser crashes.
+  for (var i = 0; i < uris.length; i++) {
+    var uri = uris[i];
+    var script = $.ajax({url: uri, async: false, dataType: 'text' }).responseText; // Fun fax: if you don't flag it as text, jQuery will "intelligently" assume it to be javascript and recursively eval the same file over and over until the browser crashes.
 
-		if (!loaded_scripts[uri]) {
-			loaded_scripts[uri] = script;
-		} else {
-			if (loaded_scripts[uri] != script) {
-				console.log("reload of " + uri);
+    if (!loaded_scripts[uri]) {
+      loaded_scripts[uri] = script;
+    } else {
+      if (loaded_scripts[uri] != script) {
+        console.log("reload of " + uri);
 
-				loaded_scripts[uri] = script;
-			}
-		}
-	}
+        loaded_scripts[uri] = script;
+      }
+    }
+  }
 };
 
 setInterval(scan, 100);
